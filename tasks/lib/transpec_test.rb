4e00d8cd468d3ebb52c038cde677eef0d06a1d5b
Yuji Nakayama <nkymyj@gmail.com>
2013-10-08 01:12:54 +0900

    Extract TranspecTest class to a file

---
+++ tasks/lib/transpec_test.rb
@@ -0,0 +1,151 @@
+# coding: utf-8
+
+class TranspecTest
+  include FileUtils # This is Rake's one.
+
+  attr_reader :url, :ref, :bundler_args
+
+  def self.base_dir
+    @base_dir ||= begin
+      base_dir = File.join('tmp', 'projects')
+
+      unless Dir.exist?(base_dir)
+        require 'fileutils'
+        FileUtils.mkdir_p(base_dir)
+      end
+
+      base_dir
+    end
+  end
+
+  def initialize(url, ref = nil, bundler_args = [])
+    @url = url
+    @ref = ref
+    @bundler_args = bundler_args
+  end
+
+  def name
+    @name ||= File.basename(url, '.git')
+  end
+
+  def project_dir
+    @project_dir ||= File.join(self.class.base_dir, name)
+  end
+
+  def run
+    require 'transpec'
+
+    puts " Testing on #{name} Project ".center(80, '=')
+
+    [%w(--force), %w(--force --no-parentheses-matcher-arg)].each do |args|
+      prepare_project
+      run_test(args)
+    end
+  end
+
+  private
+
+  def prepare_project
+    if url.start_with?('/')
+      prepare_with_local_dir
+    else
+      prepare_with_git_repo
+    end
+  end
+
+  def prepare_with_local_dir
+    if Dir.exist?(project_dir)
+      require 'fileutils'
+      FileUtils.rm_rf(project_dir)
+    end
+
+    Dir.mkdir(project_dir)
+
+    Dir.chdir(url) do
+      Dir.new('.').each do |entry|
+        next if ['.', '..', '.git', 'tmp'].include?(entry)
+        FileUtils.cp_r(entry, project_dir)
+      end
+    end
+
+    bundle_install
+  end
+
+  def prepare_with_git_repo
+    if Dir.exist?(project_dir)
+      if current_ref == ref
+        git_reset_hard
+        return
+      else
+        require 'fileutils'
+        FileUtils.rm_rf(project_dir)
+      end
+    end
+
+    git_clone
+    bundle_install
+  end
+
+  def run_test(transpec_args = [])
+    in_project_dir do
+      with_clean_bundler_env do
+        sh File.join(Transpec.root, 'bin', 'transpec'), *transpec_args
+        sh 'bundle exec rspec'
+      end
+    end
+  end
+
+  def in_project_dir(&block)
+    Dir.chdir(project_dir, &block)
+  end
+
+  def current_ref
+    in_project_dir do
+      `git describe --all`.chomp.sub(/\Aheads\//, '')
+    end
+  end
+
+  def git_reset_hard
+    in_project_dir do
+      sh 'git reset --hard'
+    end
+  end
+
+  def git_clone
+    Dir.chdir(self.class.base_dir) do
+      # Disabling checkout here to suppress "detached HEAD" warning.
+      sh "git clone --no-checkout --depth 1 --branch #{ref} #{url}"
+    end
+
+    in_project_dir do
+      sh "git checkout --quiet #{ref}"
+    end
+  end
+
+  def bundle_install
+    in_project_dir do
+      with_clean_bundler_env do
+        sh 'bundle', 'install', *bundler_args
+      end
+    end
+  end
+
+  def with_clean_bundler_env
+    if defined?(Bundler)
+      Bundler.with_clean_env do
+        # Bundler.with_clean_env cleans environment variables
+        # which are set after bundler is loaded.
+        prepare_env
+        yield
+      end
+    else
+      prepare_env
+      yield
+    end
+  end
+
+  def prepare_env
+    # Disable Coveralls.
+    ENV['CI'] = ENV['JENKINS_URL'] = ENV['COVERALLS_RUN_LOCALLY'] = nil
+  end
+end
