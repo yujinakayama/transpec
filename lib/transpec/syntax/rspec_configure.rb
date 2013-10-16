8b336d81f8bd3b15d309bc450075562ce6e45dd9
Yuji Nakayama <nkymyj@gmail.com>
2013-10-16 23:14:48 +0900

    Change code layout in RSpecConfigure

--- lib/transpec/syntax/rspec_configure.rb
+++ lib/transpec/syntax/rspec_configure.rb
@@ -7,6 +7,33 @@ require 'transpec/ast/scanner'
 module Transpec
   class Syntax
     class RSpecConfigure < Syntax
+      def self.conversion_target_node?(node)
+        return false unless node.type == :block
+        send_node = node.children.first
+        receiver_node, method_name, *_ = *send_node
+        Util.const_name(receiver_node) == 'RSpec' && method_name == :configure
+      end
+
+      def self.add_framework_configuration(type, config_method_name)
+        class_eval <<-END
+          def #{type}_syntaxes
+            #{type}_framework_configuration.syntaxes
+          end
+
+          def modify_#{type}_syntaxes!(syntaxes)
+            #{type}_framework_configuration.modify_syntaxes!(syntaxes)
+          end
+
+          def #{type}_framework_configuration
+            @#{type}_framework_configuration ||=
+              FrameworkConfiguration.new(node, :#{config_method_name}, source_rewriter)
+          end
+        END
+      end
+
+      add_framework_configuration :expectation, :expect_with
+      add_framework_configuration :mock,        :mock_with
+
       class FrameworkConfiguration
         include ::AST::Sexp
 
@@ -82,33 +109,6 @@ module Transpec
           first_arg_node.children.first
         end
       end
-
-      def self.conversion_target_node?(node)
-        return false unless node.type == :block
-        send_node = node.children.first
-        receiver_node, method_name, *_ = *send_node
-        Util.const_name(receiver_node) == 'RSpec' && method_name == :configure
-      end
-
-      def self.add_framework_configuration(type, config_method_name)
-        class_eval <<-END
-          def #{type}_syntaxes
-            #{type}_framework_configuration.syntaxes
-          end
-
-          def modify_#{type}_syntaxes!(syntaxes)
-            #{type}_framework_configuration.modify_syntaxes!(syntaxes)
-          end
-
-          def #{type}_framework_configuration
-            @#{type}_framework_configuration ||=
-              FrameworkConfiguration.new(node, :#{config_method_name}, source_rewriter)
-          end
-        END
-      end
-
-      add_framework_configuration :expectation, :expect_with
-      add_framework_configuration :mock,        :mock_with
 
       class UnknownSyntaxError < StandardError; end
     end
