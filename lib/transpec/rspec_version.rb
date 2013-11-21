c28708af40e1c32aeee9fc81518da1a283d521bb
Yuji Nakayama <nkymyj@gmail.com>
2013-11-22 04:58:27 +0900

    Sort methods in RSpecVersion

--- lib/transpec/rspec_version.rb
+++ lib/transpec/rspec_version.rb
@@ -34,20 +34,20 @@ module Transpec
       @gem_version >= GEM_VERSION_2_99_0_BETA1
     end
 
-    def receive_messages_available?
-      @gem_version >= GEM_VERSION_3_0_0_BETA1
-    end
-
-    def receive_message_chain_available?
-      @gem_version >= GEM_VERSION_3_0_0_BETA2
-    end
-
     def yielded_example_available?
       @gem_version >= GEM_VERSION_2_99_0_BETA1
     end
 
     def one_liner_is_expected_available?
       @gem_version >= GEM_VERSION_2_99_0_BETA2
+    end
+
+    def receive_messages_available?
+      @gem_version >= GEM_VERSION_3_0_0_BETA1
+    end
+
+    def receive_message_chain_available?
+      @gem_version >= GEM_VERSION_3_0_0_BETA2
     end
 
     def <=>(other)
