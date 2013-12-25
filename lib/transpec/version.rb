1c0adb46aacd92b5f00abde6be4eb51e7d991e50
Yuji Nakayama <nkymyj@gmail.com>
2013-12-26 00:10:26 +0900

    Bump up version to 1.6.0

--- lib/transpec/version.rb
+++ lib/transpec/version.rb
@@ -4,8 +4,8 @@ module Transpec
   # http://semver.org/
   module Version
     MAJOR = 1
-    MINOR = 5
-    PATCH = 1
+    MINOR = 6
+    PATCH = 0
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
