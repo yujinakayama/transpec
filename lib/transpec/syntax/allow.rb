eef17ac69dabe2422a2d02106f30c6f895aadc58
Yuji Nakayama <nkymyj@gmail.com>
2014-01-21 18:54:04 +0900

    Refactor with Mixin::ExpectBase

--- lib/transpec/syntax/allow.rb
+++ lib/transpec/syntax/allow.rb
@@ -1,46 +1,15 @@
 # coding: utf-8
 
 require 'transpec/syntax'
-require 'transpec/syntax/mixin/send'
-require 'transpec/syntax/mixin/receive_matcher_owner'
-require 'transpec/util'
+require 'transpec/syntax/mixin/expect_base'
 
 module Transpec
   class Syntax
     class Allow < Syntax
-      include Mixin::Send, Mixin::ReceiveMatcherOwner, Util
+      include Mixin::ExpectBase
 
       def self.target_method?(receiver_node, method_name)
         receiver_node.nil? && [:allow, :allow_any_instance_of].include?(method_name)
-      end
-
-      def current_syntax_type
-        :expect
-      end
-
-      def positive?
-        to_method_name = parent_node.children[1]
-        to_method_name == :to
-      end
-
-      def matcher_node
-        to_arg_node = to_node.children[2]
-        if to_arg_node.block_type?
-          to_arg_node.children.first
-        else
-          to_arg_node
-        end
-      end
-
-      def block_node
-        block_node_taken_by_method(to_node)
-      end
-
-      alias_method :subject_node, :arg_node
-      alias_method :to_node, :parent_node
-
-      def subject_range
-        subject_node.loc.expression
       end
 
       def any_instance?
--- lib/transpec/syntax/expect.rb
+++ lib/transpec/syntax/expect.rb
@@ -1,47 +1,16 @@
 # coding: utf-8
 
 require 'transpec/syntax'
-require 'transpec/syntax/mixin/send'
+require 'transpec/syntax/mixin/expect_base'
 require 'transpec/syntax/mixin/have_matcher_owner'
-require 'transpec/syntax/mixin/receive_matcher_owner'
-require 'transpec/util'
 
 module Transpec
   class Syntax
     class Expect < Syntax
-      include Mixin::Send, Mixin::HaveMatcherOwner, Mixin::ReceiveMatcherOwner, Util
+      include Mixin::ExpectBase, Mixin::HaveMatcherOwner
 
       def self.target_method?(receiver_node, method_name)
         receiver_node.nil? && [:expect, :expect_any_instance_of].include?(method_name)
-      end
-
-      def current_syntax_type
-        :expect
-      end
-
-      def positive?
-        to_method_name = parent_node.children[1]
-        to_method_name == :to
-      end
-
-      def matcher_node
-        to_arg_node = to_node.children[2]
-        if to_arg_node.block_type?
-          to_arg_node.children.first
-        else
-          to_arg_node
-        end
-      end
-
-      def block_node
-        block_node_taken_by_method(to_node)
-      end
-
-      alias_method :subject_node, :arg_node
-      alias_method :to_node, :parent_node
-
-      def subject_range
-        subject_node.loc.expression
       end
 
       def any_instance?
