c8192d8a73ccaed71049de76f46499053da72a02
Yuji Nakayama <nkymyj@gmail.com>
2014-02-14 04:22:19 +0900

    Disable SimpleCov unless envinroment variable COVERAGE is set

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,20 +26,21 @@ RSpec.configure do |config|
   end
 end
 
-require 'simplecov'
-SimpleCov.coverage_dir(File.join('spec', 'coverage'))
+if ENV['TRAVIS'] || ENV['CI'] || ENV['COVERAGE']
+  require 'simplecov'
 
-if ENV['TRAVIS']
-  require 'coveralls'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-elsif ENV['CI']
-  require 'simplecov-rcov'
-  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-end
+  if ENV['TRAVIS']
+    require 'coveralls'
+    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  elsif ENV['CI']
+    require 'simplecov-rcov'
+    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  end
 
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/vendor/bundle/'
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/vendor/bundle/'
+  end
 end
 
 Dir[File.join(File.dirname(__FILE__), 'support', '*')].each do |path|
