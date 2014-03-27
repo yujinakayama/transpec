yujinakayama/transpec

commit 32b4dfed19deadad6fe5f0697f6a0864dc86002c

Avoid confusing methods defined with #let with RSpec built-in methods
Close #55.

lib/transpec/syntax/mixin/send.rb
@@ -27,7 +27,8 @@ module Transpec
             source_location = source_location(node, runtime_data)
             return true unless source_location
             file_path = source_location.first
-            !file_path.match(%r{/rspec\-[^/]+/lib/rspec/}).nil?
+            !file_path.match(%r{/rspec\-[^/]+/lib/rspec/}).nil? &&
+              !file_path.end_with?('/memoized_helpers.rb')
           end
 
           def source_location(node, runtime_data)

spec/transpec/syntax/current_example_spec.rb
@@ -53,6 +53,33 @@ module Transpec
             CurrentExample.conversion_target_node?(send_node).should be_false
           end
         end
+
+        context 'when #example node defined with #let by user is passed' do
+          let(:source) do
+            <<-END
+              describe 'example' do
+                let(:example) { 'This is not current example object' }
+
+                it 'does something' do
+                  example
+                end
+              end
+            END
+          end
+
+          it 'unfortunately returns true ' \
+             "since it's impossible to differentiate them without runtime information" do
+            CurrentExample.conversion_target_node?(send_node).should be_true
+          end
+
+          context 'with runtime information' do
+            include_context 'dynamic analysis objects'
+
+            it 'returns false properly' do
+              CurrentExample.conversion_target_node?(send_node, runtime_data).should be_false
+            end
+          end
+        end
       end
 
       describe '#convert!' do

commit 4925f2765804032ccd4abc2f6e3cba327b5625c3

Fix a bug where conversion of one-liner #should is not working
Caused by 32b4dfe.

lib/transpec/syntax/mixin/send.rb
@@ -27,8 +27,8 @@ module Transpec
             source_location = source_location(node, runtime_data)
             return true unless source_location
             file_path = source_location.first
-            !file_path.match(%r{/rspec\-[^/]+/lib/rspec/}).nil? &&
-              !file_path.end_with?('/memoized_helpers.rb')
+            return false unless file_path.match(%r{/rspec\-[^/]+/lib/rspec/})
+            !user_defined_method?(node, runtime_data)
           end
 
           def source_location(node, runtime_data)
@@ -38,8 +38,19 @@ module Transpec
             runtime_data[target_node, source_location_key(method_name)]
           end
 
+          def user_defined_method?(node, runtime_data)
+            return unless runtime_data
+            receiver_node, method_name, *_ = *node
+            target_node = receiver_node ? receiver_node : node
+            runtime_data[target_node, user_defined_method_key(method_name)]
+          end
+
           def source_location_key(method_name)
             "#{method_name}_source_location".to_sym
+          end
+
+          def user_defined_method_key(method_name)
+            "#{method_name}_user_defined_method?".to_sym
           end
         end
 
@@ -55,6 +66,14 @@ module Transpec
 
             key = self.class.source_location_key(method_name)
             code = "method(#{method_name.inspect}).source_location"
+            rewriter.register_request(target_node, key, code, target_object_type)
+
+            key = self.class.user_defined_method_key(method_name)
+            code = <<-END.gsub(/^\s+\|/, '').chomp
+              |owner = method(#{method_name.inspect}).owner
+              |owner != RSpec::Core::ExampleGroup &&
+              |  owner.ancestors.include?(RSpec::Core::ExampleGroup)
+            END
             rewriter.register_request(target_node, key, code, target_object_type)
           end
         end

spec/transpec/dynamic_analyzer/rewriter_spec.rb
@@ -21,8 +21,8 @@ module Transpec
 
         # rubocop:disable LineLength
         let(:expected_source) do
-          <<-END
-            transpec_analyze((transpec_analyze((subject), self, "(string)_12_19", { :should_source_location => [:object, "method(:should).source_location"] }).should be(foo)), self, "(string)_12_34", { :expect_available? => [:context, "self.class.ancestors.any? { |a| a.name.start_with?('RSpec::') } && respond_to?(:expect)"] })
+          <<-'END'
+            transpec_analyze((transpec_analyze((subject), self, "(string)_12_19", { :should_source_location => [:object, "method(:should).source_location"], :should_user_defined_method? => [:object, "owner = method(:should).owner\nowner != RSpec::Core::ExampleGroup &&\n  owner.ancestors.include?(RSpec::Core::ExampleGroup)"] }).should be(foo)), self, "(string)_12_34", { :expect_available? => [:context, "self.class.ancestors.any? { |a| a.name.start_with?('RSpec::') } && respond_to?(:expect)"] })
           END
         end
         # rubocop:enable LineLength
@@ -42,8 +42,8 @@ module Transpec
 
           # rubocop:disable LineLength
           let(:expected_source) do
-            <<-END
-              transpec_analyze((transpec_analyze((subject), self, "(string)_14_21", { :should_source_location => [:object, "method(:should).source_location"] }).should), self, "(string)_14_28", { :"=~_source_location" => [:object, "method(:=~).source_location"], :expect_available? => [:context, "self.class.ancestors.any? { |a| a.name.start_with?('RSpec::') } && respond_to?(:expect)"] }) =~ transpec_analyze((<<-HEREDOC.gsub('foo', 'bar')), self, "(string)_32_61", { :enumerable_arg? => [:object, "is_a?(Enumerable)"] })
+            <<-'END'
+              transpec_analyze((transpec_analyze((subject), self, "(string)_14_21", { :should_source_location => [:object, "method(:should).source_location"], :should_user_defined_method? => [:object, "owner = method(:should).owner\nowner != RSpec::Core::ExampleGroup &&\n  owner.ancestors.include?(RSpec::Core::ExampleGroup)"] }).should), self, "(string)_14_28", { :"=~_source_location" => [:object, "method(:=~).source_location"], :"=~_user_defined_method?" => [:object, "owner = method(:=~).owner\nowner != RSpec::Core::ExampleGroup &&\n  owner.ancestors.include?(RSpec::Core::ExampleGroup)"], :expect_available? => [:context, "self.class.ancestors.any? { |a| a.name.start_with?('RSpec::') } && respond_to?(:expect)"] }) =~ transpec_analyze((<<-HEREDOC.gsub('foo', 'bar')), self, "(string)_32_61", { :enumerable_arg? => [:object, "is_a?(Enumerable)"] })
               foo
               HEREDOC
             END
@@ -64,8 +64,8 @@ module Transpec
 
           # rubocop:disable LineLength
           let(:expected_source) do
-            <<-END
-              transpec_analyze((expect { do_something }), self, "(string)_14_20", { :expect_source_location => [:context, "method(:expect).source_location"] }).to throw_symbol
+            <<-'END'
+              transpec_analyze((expect { do_something }), self, "(string)_14_20", { :expect_source_location => [:context, "method(:expect).source_location"], :expect_user_defined_method? => [:context, "owner = method(:expect).owner\nowner != RSpec::Core::ExampleGroup &&\n  owner.ancestors.include?(RSpec::Core::ExampleGroup)"] }).to throw_symbol
             END
           end
           # rubocop:enable LineLength
@@ -84,8 +84,8 @@ module Transpec
 
           # rubocop:disable LineLength
           let(:expected_source) do
-            <<-END
-              transpec_analyze((double 'something'), self, "(string)_14_32", { :double_source_location => [:context, "method(:double).source_location"] })
+            <<-'END'
+              transpec_analyze((double 'something'), self, "(string)_14_32", { :double_source_location => [:context, "method(:double).source_location"], :double_user_defined_method? => [:context, "owner = method(:double).owner\nowner != RSpec::Core::ExampleGroup &&\n  owner.ancestors.include?(RSpec::Core::ExampleGroup)"] })
             END
           end
           # rubocop:enable LineLength

spec/transpec/syntax/oneliner_should_spec.rb
@@ -11,6 +11,53 @@ module Transpec
 
       let(:record) { should_object.report.records.first }
 
+      describe '.conversion_target_node?' do
+        let(:send_node) do
+          ast.each_descendent_node do |node|
+            next unless node.send_type?
+            method_name = node.children[1]
+            next unless method_name == :should
+            return node
+          end
+          fail 'No #should node is found!'
+        end
+
+        subject { OnelinerShould.conversion_target_node?(send_node, runtime_data) }
+
+        context 'when one-liner #should node is passed' do
+          let(:source) do
+            <<-END
+              describe 'example' do
+                subject { 1 }
+                it { should == 1 }
+              end
+            END
+          end
+
+          it { should be_true }
+
+          context 'with runtime information' do
+            include_context 'dynamic analysis objects'
+            it { should be_true }
+          end
+        end
+
+        context 'when monkey-patched #should node is passed' do
+          let(:source) do
+            <<-END
+              describe 'example' do
+                subject { 1 }
+                it 'is 1' do
+                  subject.should == 1
+                end
+              end
+            END
+          end
+
+          it { should be_false }
+        end
+      end
+
       describe '#matcher_node' do
         context 'when it is taking operator matcher' do
           let(:source) do