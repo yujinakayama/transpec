181e66cef200370268ac9a1fd93cb5b8e5d3483b
Yuji Nakayama <nkymyj@gmail.com>
2014-04-05 08:26:44 +0900

    Add RSpecConfigure#expose_dsl_globally=

--- lib/transpec/syntax/rspec_configure.rb
+++ lib/transpec/syntax/rspec_configure.rb
@@ -6,16 +6,21 @@ require 'transpec/util'
 module Transpec
   class Syntax
     class RSpecConfigure < Syntax
+      require 'transpec/syntax/rspec_configure/configuration_modification'
       require 'transpec/syntax/rspec_configure/expectations'
       require 'transpec/syntax/rspec_configure/mocks'
 
-      include Util
+      include ConfigurationModification
 
       def dynamic_analysis_target?
         return false unless node && node.block_type?
         send_node = node.children.first
         receiver_node, method_name, *_ = *send_node
         const_name(receiver_node) == 'RSpec' && method_name == :configure
+      end
+
+      def expose_dsl_globally=(boolean)
+        set_configuration!(:expose_dsl_globally, boolean)
       end
 
       def expectations
@@ -28,6 +33,12 @@ module Transpec
 
       def block_arg_name
         first_block_arg_name(node)
+      end
+
+      private
+
+      def find_block_node
+        node
       end
     end
   end
