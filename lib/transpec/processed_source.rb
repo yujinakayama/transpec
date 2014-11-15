b7c8e0f7dc5167167e7c48ac6514cc106c85d2ac
Yuji Nakayama <nkymyj@gmail.com>
2014-11-15 14:37:20 +0900

    Remove fallback handling for Parser
    Now it's done in Parser itself.

--- lib/transpec/processed_source.rb
+++ lib/transpec/processed_source.rb
@@ -1,13 +1,6 @@
 # coding: utf-8
 
-begin
-  require 'parser/current'
-rescue NotImplementedError
-  warn 'Falling back to Ruby 2.1 parser.'
-  require 'parser/ruby21'
-  Parser::CurrentRuby = Parser::Ruby21 # rubocop:disable ConstantName
-end
-
+require 'parser/current'
 require 'transpec/ast/builder'
 
 module Transpec
